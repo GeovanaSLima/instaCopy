23be0ffd986e8f6ad2f18675258423554983f259
geovana <geovanasslima@gmail.com>
2024-04-07 03:12:48 -0300

    move components and update imports

--- App.js
+++ App.js
@@ -1,52 +1,70 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet, FlatList, Image} from "react-native";
 
-import Header from "./src/Header";
+import Header from "./src/components/Header";
+import List from "./src/components/List";
 
 export default function App() {
   const [feed, setFeed] = useState([
     {
       id: '1', 
-      nome: 'Geovana Sousa', 
+      name: 'GeovanaSousa', 
       imgProfile: 'https://raw.githubusercontent.com/GeovanaSLima/GeovanaSLima/main/GitProfile.png',
-      imgPubli: 'src/img/publis/publi_1.jpg', 
+      imgPubli: require('./src/img/publis/publi_1.jpg'), 
       description: 'Ontem demorei para dormir, tava meio assim sla...',
       liked: true,
-      likers: 1
+      likes: 32,
+      comments: 7,
+      date: Date('2024-03-28') 
     },
     {
       id: '2', 
-      nome: 'Geovana Sousa', 
-      imgProfile: 'https://raw.githubusercontent.com/GeovanaSLima/GeovanaSLima/main/GitProfile.png',
-      imgPubli: 'src/img/publis/publi_2.jpg', 
+      name: 'geh.s', 
+      imgProfile: 'https://i.pinimg.com/236x/f3/85/d7/f385d78eba93e8b768bcc04bf96fe5a5.jpg',
+      imgPubli: require('./src/img/publis/publi_2.jpg'), 
       description: 'O meu lugar...',
-      liked: true,
-      likers: 1
+      liked: false,
+      likes: 12,
+      comments: 0,
+      date: Date('2024-03-30')
     },
     {
       id: '3', 
-      nome: 'Geovana Sousa', 
-      imgProfile: 'https://raw.githubusercontent.com/GeovanaSLima/GeovanaSLima/main/GitProfile.png',
-      imgPubli: 'src/img/publis/publi_3.jpg', 
+      name: 'geeshhl', 
+      imgProfile: 'https://i.redd.it/5560va6tsg191.jpg',
+      imgPubli: require('./src/img/publis/publi_3.jpg'), 
       description: 'Deixa eu te amar...',
       liked: true,
-      likers: 1
+      likes: 65,
+      comments: 5,
+      date: Date('2024-04-02')
     },
     {
       id: '4', 
-      nome: 'Geovana Sousa', 
-      imgProfile: 'https://raw.githubusercontent.com/GeovanaSLima/GeovanaSLima/main/GitProfile.png',
-      imgPubli: 'src/img/publis/publi_4.jpg', 
+      name: 'J.gsl', 
+      imgProfile: 'https://miro.medium.com/v2/resize:fit:400/1*B8c1ED3QV_yaa6PAWqDgMw.png',
+      imgPubli: require('./src/img/publis/publi_4.jpg'), 
       description: "2 Seconds without you is like 2 much...",
       liked: true,
-      likers: 1
-    }
+      likes: 89,
+      comments: 15,
+      date: Date('2024-04-05')
+    },
   ])
 
 
   return(
     <View style={StyleSheet.container}>
+
       <Header/>
+
+      <FlatList 
+      data={feed}
+      keyExtractor={ (item) => item.id }
+      renderItem={ ({ item }) => <List data={item} /> }
+      showsVerticalScrollIndicator={false}
+      />
+
     </View>
   );
 }
