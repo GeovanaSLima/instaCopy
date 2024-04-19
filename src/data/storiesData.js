db0e9272ef8b1996f59ce40ab5a0bef65d16cfc9
geovana <geovanasslima@gmail.com>
2024-04-18 22:08:50 -0300

    change user name

--- src/data/storiesData.js
+++ src/data/storiesData.js
@@ -2,7 +2,7 @@
 export const stories = [
     {
         id: '1', 
-        name: 'geehSl', 
+        name: 'Your Story', 
         imgProfile: 'https://i.pinimg.com/236x/f3/85/d7/f385d78eba93e8b768bcc04bf96fe5a5.jpg',
         watched: true,
         storiesType: 'closeFriends',
