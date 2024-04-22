aec6467f552510031e449c7dd1d962f618fb1a53
geovana <geovanasslima@gmail.com>
2024-04-21 21:19:33 -0300

    Refactor RecentSearch component

---
+++ src/components/RecentSearch/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
+
+export default function RecentSearchScreen() {
+    return(
+        <View>
+            <Text>Recent Search</Text>
+        </View>
+    );
+}
